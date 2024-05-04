0e2a05e7f243c3118c27d22ab1b4451faec549cd
Jegor <140427023+jegorRegularUser@users.noreply.github.com>
2024-05-04 22:06:58 +0300

    Реализованы операции с деньгами, работа с избранным

--- public/homePage.js
+++ public/homePage.js
@@ -1,14 +1,92 @@
 const logOutButton = new LogoutButton()
 logOutButton.action = () => {
-    ApiConnector.logout(res => { if (res.success) location.reload() })
+    ApiConnector.logout(res => {
+        if (res.success) location.reload()
+    })
 }
 ApiConnector.current((res) => {
     ProfileWidget.showProfile(res.data);
 });
 
+
 const ratesBoard = new RatesBoard()
-ratesBoard.getCurrentStocks = ()=>{ ApiConnector.getStocks((res) => {if (res.success){ratesBoard.clearTable; ratesBoard.fillTable(res.data)} })}
+ratesBoard.getCurrentStocks = () => {
+    ApiConnector.getStocks((res) => {
+        if (res.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(res.data)
+        }
+    })
+}
+
 ratesBoard.getCurrentStocks()
 setInterval(() => {
     ratesBoard.getCurrentStocks();
 }, 60000);
+
+
+const moneyManager = new MoneyManager();
+
+const favoritesWidget = new FavoritesWidget();
+
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, res => {
+        if (res.success) {
+            ProfileWidget.showProfile(res.data);
+            favoritesWidget.setMessage(res.success, 'Пополнение прошло успешно')
+        }
+        else favoritesWidget.setMessage(res.success, 'Пополнение не удалось')
+    })
+}
+
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, res => {
+        if (res.success) {
+            ProfileWidget.showProfile(res.data);
+            favoritesWidget.setMessage(res.success, 'Конвертация прошла успешно')
+        }
+        else favoritesWidget.setMessage(res.success, 'Конвертация не удалась')
+    })
+}
+
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, res => {
+        if (res.success) {
+            ProfileWidget.showProfile(res.data);
+            favoritesWidget.setMessage(res.success, 'Перевод прошел успешно')
+        }
+        else favoritesWidget.setMessage(res.success, 'Перевод не удался')
+    })
+}
+
+ApiConnector.getFavorites((res) => {
+    if (res.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(res.data);
+        moneyManager.updateUsersList(res.data);
+    }
+});
+
+favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, res => {
+        if (res.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(res.data);
+            moneyManager.updateUsersList(res.data);
+            favoritesWidget.setMessage(res.success, 'Пользователь добавлен успешно')
+        }
+        else favoritesWidget.setMessage(res.success, 'Не удалось добавить пользователя')
+    })
+}
+
+favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, res => {
+        if (res.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(res.data);
+            moneyManager.updateUsersList(res.data);
+            favoritesWidget.setMessage(res.success, 'Пользователь удален успешно')
+        }
+        else favoritesWidget.setMessage(res.success, 'Не удалось удалить пользователя')
+    })
+}
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,8 +1,18 @@
 'use strict'
 const user = new UserForm()
-user.loginFormCallback = (data) => {
-    ApiConnector.login(data, (res) => { if (res.success) { location.reload() } else alert(res.error) })
+user.loginFormCallback = data => {
+    ApiConnector.login(data, (res) => {
+        if (res.success) {
+            location.reload()
+        }
+        else alert(res.error)
+    })
 }
-user.registerFormCallback = (data) => {
-    ApiConnector.register(data, (res) => { if (res.success) { location.reload() } else alert(res.error) })
+user.registerFormCallback = data => {
+    ApiConnector.register(data, (res) => {
+        if (res.success) {
+            location.reload()
+        }
+        else alert(res.error)
+    })
 }
